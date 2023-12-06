be7b4d2f24f8296d38b893746a319cc1dfc2cf87
senichek <senichek@gmail.com>
2023-12-06 23:56:13 +0100

    front end basic setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
+import trainImg from './train.jpg'
+import TrainIcon from '@mui/icons-material/Train';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  const respond = () => {
-    console.log("Responded");
-  }
+  const [message, setMessage] = useState("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+  const [showMessage, setShowMessage] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={respond}>Cheer me up!</button>
-      </header>
+    <div className='container'>
+      <img src={trainImg} className="main-img" alt="image_of_train" />
+      <div className='train-icon' onClick={() => setShowMessage(!showMessage)}>
+        <TrainIcon />
+      </div>
+      <div className='footer'>Click the train to see an interesting message</div>
+      {showMessage &&
+        <div className='message'>{message}</div>
+      }
     </div>
   );
 }
