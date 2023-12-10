89d9f7caf5e4799e3b396ff7a633d9624e76e6e9
senichek <senichek@gmail.com>
2023-12-10 14:31:15 +0100

    added fetch request

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import { useState } from 'react';
 
 function App() {
 
-  const [message, setMessage] = useState("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("");
+  const [msgCounter, setMsgCounter] = useState(0);
+
+  const getRespone = async () => {
+    const response = await fetch("https://dxx4gq4ir5.execute-api.us-east-1.amazonaws.com/dev/cheer/cheermeup");
+    const data = await response.json();
+    setMessage(data);
+    setMsgCounter(msgCounter + 1);
+    console.log(data);
+  }
 
   return (
     <div className='container'>
       <img src={trainImg} className="main-img" alt="image_of_train" />
-      <div className='train-icon' onClick={() => setShowMessage(!showMessage)}>
+      {/* <div className='train-icon' onClick={() => setShowMessage(!showMessage)}> */}
+      <div className='train-icon' onClick={() => getRespone()}>
         <TrainIcon />
       </div>
-      <div className='footer'>Click the train to see an interesting message</div>
-      {showMessage &&
-        <div className='message'>{message}</div>
+      <div className='footer'>Click the train icon to see an AI-generated message</div>
+      {message &&
+        <div className='message'>{`Msg ${msgCounter}: ${message}`}</div>
       }
     </div>
   );
